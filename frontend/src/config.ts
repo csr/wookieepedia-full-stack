6b8ac1da7ec15dd17e6e1ecbca5e7f1973d32f16
Cesare De Cal <cesare.de.cal@ericsson.com>
2024-07-02 21:56:35 +0200

    update env variable

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,8 @@
 const config = {
-    backendAppApiUrl: process.env.REACT_APP_BACKEND_APP_API_BASE_URL,
-    backendAppApiPrefix: process.env.REACT_APP_BACKEND_APP_API_PREFIX,
+    // Passing the hosting URL as an environment variable would be ideal, but for some reason
+    // the app isn't able to parse the environment variable correctly. 
+    backendAppApiUrl: process.env.REACT_APP_BACKEND_APP_API_BASE_URL || "https://wookieepedia-fullstack-app-grbeu.ondigitalocean.app",
+    backendAppApiPrefix: process.env.REACT_APP_BACKEND_APP_API_PREFIX || "v1",
 };
   
 export default config;
